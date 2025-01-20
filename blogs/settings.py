c982a58edcee3ca2e812cc65f4e7d54bc0a81688
magalaReuben <magalareuben60@gmail.com>
2025-01-21 01:43:27 +0300

    Field relations fixes

--- blogs/settings.py
+++ blogs/settings.py
@@ -137,5 +137,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
